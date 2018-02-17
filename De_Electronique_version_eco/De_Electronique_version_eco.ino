8d08e409b3c06bc3520552102583d7f07ae5ed8a
Oasis266 <sucrealin@hotmail.fr>
2018-02-17 02:25:41 +0100

    netoyage
    reduction

--- De_Electronique_version_eco/De_Electronique_version_eco.ino
+++ De_Electronique_version_eco/De_Electronique_version_eco.ino
@@ -11,37 +11,33 @@ int pinLeds2 = 1;
 int pinLeds3 = 2;
 int pinLed4 = 3; 
 
-// on indique la pin du capteur d'inclinaison
-int buttonPin = 4; 
-// On vérifie l'état du capteur d'inclinaison
-int buttonState;
-// Variable pour le random de l'animation 
-long ranim;
-// Variable pour le random du résultat final
-long ran;
-// Il n'y a pas encore eu de lancement de dé
-int last = 0;
-// Temps que le dé reste affiché 10000 ms = 10 seconde
-int timo = 10000; 
+                    
+int buttonPin = 4;  // on indique la pin du capteur d'inclinaison
+
+int buttonState;   // On vérifie l'état du capteur d'inclinaison
+                           
+long ranim;      // Variable pour le random de l'animation 
+         
+    
+long ran;           // Variable pour le random du résultat final
+
+
+int last = 0;     // Il n'y a pas encore eu de lancement de dé
+                    
+int timo = 10000;  // Temps que le dé reste affiché 10000 ms = 10 seconde
 
 volatile uint8_t wdt_count;  // Incremented by watchdog interrupt <----------
-
 
 // ----- DECLARATION ENTREES ET SORTIES ------
 void setup ()
 {
-      //arret des services
-                                                    // ADCSRA &= ~_BV(ADEN);  // switch ADC OFF     
+      //arret des services   
   ACSR  |= _BV(ACD);     // switch Analog Compartaror OFF
-
 
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);// Configure attiny85 sleep mode
   // Reset watchdog interrupt counter
   wdt_count = 255; //max value
 
-
-
-  
   //On indique que les LED sont des sorties
   pinMode (pinLeds1, OUTPUT);
   pinMode (pinLeds2, OUTPUT);
@@ -50,31 +46,29 @@ void setup ()
   // On indique que le detecteur d'inclinaison est une entrée.
   pinMode (buttonPin, INPUT);
 
-  randomSeed(analogRead(0)); // on initialise le fait de pouvoir lancer des randoms
-  
+   // on initialise le fait de pouvoir lancer des randoms
+ randomSeed(analogRead(0));
   
 }
-
-
-
-
 
 // ----- ACTIONS A EFFECTUER ------
 
 void blink(uint8_t flash)
-//void loop()
+
 {  
   buttonState = digitalRead(buttonPin);
-  if (buttonState == LOW){ // Si on bouge le dé
-    
-   for (int i=0; i <= 7; i++){ // Animation pour 8 affichages de dé avec 200 ms entre chaque
+ if (buttonState == HIGH){              // Si on bouge le dé  
+
+   for (int i=0; i <= 7; i++){             // Animation pour 8 affichages de dé avec 200 ms entre chaque  
      
-  ranim = random(1, 7); // on effectue un random pour chaque affichage
-  
-  while (ranim == last){ // On verifie que deux affichages consécutifs ne sont pas les mêmes 
-    ranim = random(1, 7); // Sinon on random un autre chiffre
-                       }  
+  ranim = random(1, 7);                   // on effectue un random pour chaque affichage
+ 
+  while (ranim == last){                   // On verifie que deux affichages consécutifs ne sont pas les mêmes 
+    ranim = random(1, 7);                 // Sinon on random un autre chiffre
+           }  
   last = ranim; // On stock le chiffre pour éviter d'afficher les 2 mêmes valeurs de dé pendant l'animation
+   
+    
     if (ranim == 1){
       digitalWrite (pinLed4, HIGH);
     }
@@ -107,10 +101,11 @@ void blink(uint8_t flash)
       delay(50);
                           } 
     
-    
+  
 // RANDOM DU RESULTAT FINAL
-    ran = random(1, 7); 
-    if (ran == 1){
+        ran = random(1, 7);
+    
+   if (ran == 1){
       digitalWrite (pinLed4, HIGH);
       delay(timo); // time étant la variable qui vaut 7000ms donc affichage du résultat pendant 7 secondes
     }
@@ -148,35 +143,26 @@ void blink(uint8_t flash)
   digitalWrite (pinLed4, LOW);
   
 }  
-
-
 void loop() {  
                //on lance le code lumiere
+              
              blink(3);
              wdt_count = 0;
              watchdog_start_interrupt(6);      // prescale of 6 ~= 1sec //on lance les interruptions cpu
-                
-           
-        
-
-               if(buttonState == LOW) {       // si le capeteur et enclenché
-                                           watchdog_stop();
-                                           ADCSRA |= (1<<ADEN); //Enable ADC
-                                       }
-                                      else{       // veille en atendant signal de mise en marche
-                                          ADCSRA &= ~(1<<ADEN); //Disable ADC, saves ~230uA
-                                            sleep_mode(); // Make CPU sleep until next WDT interrupt
-                                            }
-                             
-              }
-
-              
+       if(buttonState == HIGH) {       // si le capeteur et enclenché
+                                watchdog_stop();
+                                ADCSRA |= (1<<ADEN); //Enable ADC
+                               }
+                          else{       // veille en atendant signal de mise en marche
+                              ADCSRA &= ~(1<<ADEN); //Disable ADC, saves ~230uA
+                               sleep_mode(); // Make CPU sleep until next WDT interrupt
+                             }
+        }           
 /* Turn off WDT */
 void watchdog_stop() {
   WDTCR |= _BV(WDCE) | _BV(WDE);
   WDTCR = 0x00;
 }
-
 /* Turn onn WDT (with interupt) */
 void watchdog_start_interrupt(uint8_t wd_prescaler) {
   if(wd_prescaler > 9) wd_prescaler = 9;
@@ -185,18 +171,11 @@ void watchdog_start_interrupt(uint8_t wd_prescaler) {
   // ^ fourth bit of the prescaler is somewhere else in the register...
   // set new watchdog timer prescaler value
   WDTCR = _prescaler;
-
   // start timed sequence
   WDTCR |= _BV(WDIE) | _BV(WDCE) | _BV(WDE);
 }
-
 // Watchdog Interrupt Service / is executed when watchdog timed out
 ISR(WDT_vect) {
   wdt_count++;
   WDTCR |= _BV(WDIE);           // Watchdog goes to interrupt not reset
 }
-
-
-
-
-
